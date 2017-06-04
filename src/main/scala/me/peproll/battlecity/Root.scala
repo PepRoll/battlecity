e30103ccc035ffe1634818185ef2744c2481b941
PepRoll <peproll@protonmail.com>
2017-06-04 23:46:36 +0300

    surface added

--- src/main/scala/me/peproll/battlecity/Root.scala
+++ src/main/scala/me/peproll/battlecity/Root.scala
@@ -6,8 +6,17 @@ import japgolly.scalajs.react.vdom.html_<^._
 
 object Root {
 
+  private val height = 600.px
+  private val width = 800.px
+
   val component = ScalaComponent.builder[Unit]("Battle city")
-  .renderStatic(<.div("Battle city"))
-  .build
+    .renderStatic(
+      <.canvas(
+        ^.width := width,
+        ^.height := height,
+        ^.background := "url('/../../src/main/resources/images/surface.jpg')"
+      )
+    )
+    .build
 
 }
